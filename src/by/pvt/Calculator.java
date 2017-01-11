133292c3bdf2347bc9d96ea3e1521a0b07575546
Nixon <Vladko-91@tyt.by>
2017-01-11 20:07:14 +0300

    add class

--- src/by/pvt/Calculator.java
+++ src/by/pvt/Calculator.java
@@ -5,6 +5,16 @@ package by.pvt;
  */
 public class Calculator {
     public static void main(String[] args) {
-
+            String s = args[0];
+            char [] ch = s.toCharArray();
+            String str1 = "";
+            String str2 = "";
+            String operator = "";
+            char charTmp = ' ';
+            int count = 0;
+            int countFirsDigit = 0;
+            int countSecondDigit= 0;
+            double x = 0;
+            double y = 0;
     }
 }
