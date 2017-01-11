de342f6caa030671da8df374d3240d8899888255
behemoth-13 <behmoth-13@yandex.ru>
2017-01-11 19:18:53 +0300

    Start

--- src/by/pvt/Calculator.java
+++ src/by/pvt/Calculator.java
@@ -1,10 +1,21 @@
 package by.pvt;
 
+import java.util.Scanner;
+
 /**
  * Created by AD13 on 11.01.2017.
  */
 public class Calculator {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        double a = scanner.nextDouble();
+        double b = scanner.nextDouble();
+        double result = divide(a, b);
+        System.out.println(result);
     }
+
+    static double divide(double a, double b){
+        return a/b;
+    }
+
 }
